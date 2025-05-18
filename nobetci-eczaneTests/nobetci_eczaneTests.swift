9b210b78b4b37d5f67b44f183dccff92fc3c49d0
kesermac <keserbaros1@hotmail.com>
2025-05-18 19:30:48 +0300

    Initial Commit

---
+++ nobetci-eczaneTests/nobetci_eczaneTests.swift
@@ -0,0 +1,17 @@
+//
+//  nobetci_eczaneTests.swift
+//  nobetci-eczaneTests
+//
+//  Created by kesermac on 18.05.2025.
+//
+
+import Testing
+@testable import nobetci_eczane
+
+struct nobetci_eczaneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
